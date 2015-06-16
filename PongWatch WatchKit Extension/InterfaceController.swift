8695ba0859f4de2add2c2e99f3205afb2a5fb570
Andrew H <andrew@andrews-imac.local>
2015-06-15 22:28:49 -0400

    add notice to file

--- PongWatch WatchKit Extension/InterfaceController.swift
+++ PongWatch WatchKit Extension/InterfaceController.swift
@@ -5,8 +5,17 @@
 //  Created by Andrew H on 6/11/15.
 //  Copyright © 2015 Andrew H. All rights reserved.
 //
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
 
-/* 
+/*
 
 This game should:
 - Have two moving paddles, one for the player and another autonomous computer one
